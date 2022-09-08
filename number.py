36d0df3992afc00edb9e4d61731715fb910dab2b
UnPicanteMal <matiasdoreiro@gmail.com>
2022-09-07 23:03:16 -0300

    .\number.py

--- number.py
+++ number.py
@@ -1,8 +1,12 @@
-while True:
-    try:
-        x = int(input("What's x? "))
-        break
-    except ValueError:
-        print("x is not a number")
+def main():
+    x = get_int("What's x? ")
+    print(f"x is {x}")
 
-print(f"x is {x}")
+def get_int(prompt):
+    while True:
+        try:
+            return int(input(prompt))
+        except ValueError:
+            pass
+
+main()
